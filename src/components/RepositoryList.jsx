858b228be60575329c532d208c85bc69ad607dab
Yago Gomes <yagogomesc@gmail.com>
2021-05-05 16:28:24 -0300

    First commit

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,28 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+import "../styles/repositories.scss";
+import { useEffect, useState } from "react";
+
+export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/yagogomesc/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+
+      <ul>
+        {repositories.map((repository) => {
+          return (
+            <RepositoryItem key={repository.name} repository={repository} />
+          );
+        })}
+      </ul>
+    </section>
+  );
+}
